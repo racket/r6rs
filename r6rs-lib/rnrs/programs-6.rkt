f5e0ddb687c050fec6ecc9786155a40ac3d1cd8f
Matthew Flatt <mflatt@racket-lang.org>
2022-07-24 14:46:24 -0600

    fix `(exit #f)`
    Repair suggested by @soegaard
    
    Closes #9

--- r6rs-lib/rnrs/programs-6.rkt
+++ r6rs-lib/rnrs/programs-6.rkt
@@ -2,8 +2,16 @@
 
 (require scheme/mpair)
 
-(provide command-line exit)
+(provide command-line
+         (rename-out [r6rs-exit exit]))
 
 (define (command-line)
   (mcons (path->string (find-system-path 'run-file))
          (list->mlist (vector->list (current-command-line-arguments)))))
+
+(define r6rs-exit
+  (let ()
+    (lambda ([x 0])
+      (if x
+          (exit x)
+          (exit 1)))))
